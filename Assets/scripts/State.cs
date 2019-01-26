64e2f3ae84a22b03c6559965b17fcc77bc5d6241
Borowiecki, Emil <emil.borowiecki@sap.com>
2019-01-26 18:12:44 +0100

    Create 3 example levels

--- Assets/scripts/State.cs
+++ Assets/scripts/State.cs
@@ -8,12 +8,13 @@ public class State : ScriptableObject
     public List<Level> levels = new List<Level>();
     public int currentLevel = 0;
     public int nextLevel = 1;
-    public int stepsVariations = 3;
     public GameObject background;
 
     private void OnEnable()
     {
         //public Level(int steps, int stepsVar, float stepDist, float stepDistRand, float route, float routeWidthRand)
         levels.Add(new Level(8, 3, 0.9f, 0.05f, 0.7f, 0.1f));
+        levels.Add(new Level(12, 3, 1.3f, 0.05f, 0.7f, 0.1f));
+        levels.Add(new Level(16, 3, 1.1f, 0.05f, 0.7f, 0.1f));
     }
 }
