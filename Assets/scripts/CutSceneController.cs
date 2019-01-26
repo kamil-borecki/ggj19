aca286e64222425e7007d5b119bad52620430f8f
Borowiecki, Emil <emil.borowiecki@sap.com>
2019-01-26 12:34:27 +0100

    [Video] Setup video player

---
+++ Assets/scripts/CutSceneController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutSceneController : MonoBehaviour
+{
+    public State state;
+
+    // Use this for initialization
+    void Start()
+    {
+        string url = "Assets/Resources/lvl" + state.currentLevel + "/video/1.mp4";
+        gameObject.GetComponent<UnityEngine.Video.VideoPlayer>().url = url;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
